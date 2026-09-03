ff5b95829e96302055c38482bff1cdadd7918dda
CDiangell-dei <dev@amazonaco.com.br>
2026-09-02 21:34:26 -0400

    chore: padronizacao do rodape institucional corporativo Amazon Aco em todas as telas

--- service_worker_pwa.js
+++ service_worker_pwa.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portal-estoques-v8.3-mobile-cards-bi-depara';
+const CACHE_NAME = 'portal-estoques-v8.4-rodape-institucional';
 const STATIC_ASSETS = [
   './',
   './index.html',
